5dc309805855975ce70fc20cb77a5f9d6edb0a18
Jack0428-dream <192861219+Jack0428-dream@users.noreply.github.com>
2025-11-06 21:41:22 +0900

    add likedlist class

--- main.js
+++ main.js
@@ -1,14 +1,121 @@
-if (index < 0 || index >= buckets.length) {
-    throw new Error("Trying to access index out of bounds");
+// if (index < 0 || index >= buckets.length) {
+//     throw new Error("Trying to access index out of bounds");
+// }
+class Node {
+
+    constructor(value = null, next = null) {
+        this.value = value;
+        this.next = next;
+    }
 }
+
+class Linkedlist {
+
+    constructor() {
+        this.head = null;
+    }
+
+    append(value) {
+        let node = new Node(value, null);
+
+        if(this.head === null) {
+            this.head = node;
+        } else {
+            let current = this.head;
+
+            while(current.next) {
+                current = current.next;
+            }
+
+            current.next = node;
+        }
+    }
+
+    size() {
+        if(this.head === null) {
+            return 0
+        } else {
+            let current = this.head;
+            let total = 0;
+
+            while(current.next) {
+                total += 1;
+
+                current = current.next;
+            }
+            total += 1;
+
+            return total
+        }
+    }
+
+    head() {
+        if(this.head === null) {
+            return null
+        } else {
+            return this.head.value;
+        }
+    }
+
+    tail() {
+        if(this.head === null) {
+            return null
+        } else {
+            let current = this.head;
+
+            while(current.next) {
+                current = current.next;
+            }
+
+            return current.value;
+        }
+    }
+
+    pop() {
+        let current = this.head;
+        
+        for(let i = 0; i <= this.size()-2; i++) {
+            current = current.next;
+        }
+
+        current.next = null;
+    }
+
+    contains(value) {
+        let current = this.head;
+
+        for(let i = 0; i < this.size(); i++) {
+            if(current.value === value) {
+                return current.value;
+            } else {
+                current = current.next;
+            }
+        }
+    }
+
+    toString() {
+        let current = this.head;
+        let base = "-> null";
+        let string = "";
+
+        for(let i = 0; i < this.size(); i++) {
+            string += `(${current.value}) -> `
+
+            current = current.next;
+        }
+
+        string.concat(base);
+    }
+}
+
 
 class HashMap {
     
     constructor() {
         this.initial = 16;
+        this.buckets = new Array(this.initial).fill(null);
         this.loadFacotr = 0.75;
-        this.capacity = buckets.length * 0.75
-        this.buckets = new Array(initial).fill(null);
+        this.capacity = this.buckets.length * 0.75
     }
 
     hash(key) {
@@ -26,7 +133,19 @@ class HashMap {
     // chance of collision up -> use modulo % on each iteration instead of outside the loop at the end
 
     set(key, value) {
+        if(this.buckets.length === Math.trunc(this.capacity)) {
+            for(let i = 0; i <= this.buckets.length; i++) {
+                this.buckets.push(null);
+            }
+        }
 
+        let index = this.hash(key);
+
+        if(this.buckets[index][0] === key) {
+            this.buckets[index][1] = value;
+        } else if(this.buckets[index][0] !== key) {
+
+        } 
     }
 
     get(key) {
@@ -58,3 +177,5 @@ class HashMap {
     }
 
 }
+
+
